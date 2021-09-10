8458dd78dca61e35cb93eff91bb2f4fed323b2ab
Webeggin <65745074+Webeggin@users.noreply.github.com>
2021-09-10 16:55:02 +0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+﻿ const placeHolderDiv = document.getElementById("modalPlaceHolder");
+ const btnAdd = document.getElementById("btnAdd");
+ btnAdd.addEventListener("click", function () {
+            fetch('/Category/Add').then(function (response) {
+                return response.text();
+            }).then(function (data) {
+                placeHolderDiv.innerHTML = data;
+                placeHolderDiv.querySelector(".modal").classList.add('show', 'd-block');
+                const btnCls = placeHolderDiv.querySelector(".btnCls");
+                btnCls.addEventListener("click", function () {
+                    placeHolderDiv.querySelector(".modal").classList.remove('show', 'd-block');
+                });
+            });
+        });
